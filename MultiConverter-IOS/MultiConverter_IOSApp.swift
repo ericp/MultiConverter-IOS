d1b45e797499e6173c36c429597388d45942c4c4
ericp <eric.pattinson@hotmail.com>
2024-02-06 11:20:35 +0100

    Initial Commit

---
+++ MultiConverter-IOS/MultiConverter_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiConverter_IOSApp.swift
+//  MultiConverter-IOS
+//
+//  Created by Eric on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiConverter_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiConverter-IOSUITests/MultiConverter_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MultiConverter_IOSUITestsLaunchTests.swift
+//  MultiConverter-IOSUITests
+//
+//  Created by Eric on 6/2/24.
+//
+
+import XCTest
+
+final class MultiConverter_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
